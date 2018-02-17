bfac8e03452d5010c5a04fff678c87f0e5fc67e4
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2018-02-17 16:04:35 +0100

    nr: Add our settings app
    Change-Id: I53afffd3578ed0c1ba5d7eb1eb0e3309c32a8b9c
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -191,4 +191,7 @@ public class Settings extends SettingsActivity {
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // NucleaRom settings
+    public static class NrSettingsActivity extends SettingsActivity {}
+
 }
